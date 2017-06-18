659994299f7027278d5da348bcef091c80a45fba
Francesco Rodriguez <frodsan@protonmail.com>
2017-06-18 12:11:36 +0200

    Rename test package.

--- rom_test.go
+++ rom_test.go
@@ -1,13 +1,15 @@
-package rom
+package rom_test
 
 import (
 	"testing"
+
+	"github.com/frodsan/rom"
 )
 
 const URL = "redis://127.0.0.1:6379/13"
 
 func TestFlushDB(t *testing.T) {
-	db := Connect(URL)
+	db := rom.Connect(URL)
 
 	db.Flush()
 }
